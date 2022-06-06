dfc8761a4786f1efb4adee3f6e2da4c99e7aa689
SamarpitSantoki <samarpit.santoki@gmail.com>
2022-06-06 08:28:12 +0530

    Fixed cors error

--- server/app.js
+++ server/app.js
@@ -1,19 +1,22 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
-const mkdir = require('mkdir')
-const fs = require('fs-extra')
+const mkdir = require("mkdir");
+const fs = require("fs-extra");
+const cors = require("cors");
 //intialize app
 const app = express();
-
+app.use(cors());
 dotenv.config({ path: "./.env" });
 const PORT = parseInt(process.env.PORT) || 3000;
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true,
-}))
+  })
+);
 
-require('./config/db')
+require("./config/db");
 require("./router.js")(app);
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
